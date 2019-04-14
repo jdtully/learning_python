f847531f6416532db342b3924985417b7c150271
jdtully <jefftully@gmail.com>
2019-04-14 16:18:56 -0400

    added wtf

---
+++ wtf.py
@@ -0,0 +1,20 @@
+months = {
+    "jan":"January",
+    "feb":"February",
+    "mar":"March",
+    "apr":"April",
+    "may":"May",
+    "jun":"June",
+    "jul":"July",
+    "aug":"August",
+    "sep":"September",
+    "oct":"October",
+    "nov":"November",
+    "dec":"December",
+    
+}
+
+monthInput = input("Name a month ")
+monthLower = monthInput.lower()
+print(months[monthLower])
+"adfsdfsdfa"
