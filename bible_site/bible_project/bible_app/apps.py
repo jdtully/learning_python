7660b9ff06dbbaf3d5f6a27be33af34cb8216075
jdtully <jefftully@gmail.com>
2019-05-09 09:36:11 -0400

    trying to catch up

---
+++ bible_site/bible_project/bible_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BibleAppConfig(AppConfig):
+    name = 'bible_app'
---
+++ bible_site/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\admin\documents\learning_python\bible_site\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
